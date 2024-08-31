90ff1b120c4d773490788ccd6095f3e17b5cfa06
Pratiksha7090 <pratikshashirdone10@gmail.com>
2024-08-31 23:08:17 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -2,7 +2,25 @@
 #define SOUNDEX_H
 
 #include <string>
+#include <unordered_map>
+#include <cctype>
+#include <array>
 
-std::string generateSoundex(const std::string& name);
+struct SoundexMapping {
+    char letter;
+    char code;
+};
+
+struct Soundex {
+    static std::string generate(const std::string& name);
+
+private:
+    static char getSoundexCode(char c);
+    static bool isVowelOrIgnored(char c);
+    static std::string padWithZeroes(const std::string& soundex, std::size_t targetLength = 4);
+};
+
+// Utility function for initializing the Soundex map
+std::unordered_map<char, char> initializeSoundexMap();
 
 #endif // SOUNDEX_H
