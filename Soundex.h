fb9ecc736a6bff7e900502679a2c4da8d9be086f
Pratiksha7090 <pratikshashirdone10@gmail.com>
2024-09-05 11:01:27 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -4,23 +4,30 @@
 #include <string>
 #include <unordered_map>
 #include <cctype>
-#include <array>
 
-struct SoundexMapping {
-    char letter;
-    char code;
-};
+using SoundexMap = std::unordered_map<char, char>;
 
 struct Soundex {
     static std::string generate(const std::string& name);
 
 private:
+    static const SoundexMap mapping;
+
+    // Helper methods for SRP
+    static std::string initializeSoundex(const std::string& name);
+    static void processNameCharacters(const std::string& name, std::string& soundex);
+    
+    static bool shouldProcessCharacter(char currentChar, char prevCode);
+    static void addCharacterToSoundex(std::string& soundex, char currentChar, char& prevCode);
+
+    static bool shouldSkipCharacter(char c);
+    static bool isValidNewCode(char currentChar, char prevCode);
+};
+
+struct SoundexUtils {
     static char getSoundexCode(char c);
     static bool isVowelOrIgnored(char c);
     static std::string padWithZeroes(const std::string& soundex, std::size_t targetLength = 4);
 };
-
-// Utility function for initializing the Soundex map
-std::unordered_map<char, char> initializeSoundexMap();
 
 #endif // SOUNDEX_H
